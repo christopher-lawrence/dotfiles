ce00066a95b9a46e770c5c746e966b060625a3ca
Christopher Lawrence <chrislawrence05@gmail.com>
2023-08-24 11:37:56 -0400

    massive cleanup

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,31 +3,30 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'lua_ls',
-	'tsserver',
+  'lua_ls',
+  'tsserver',
   'omnisharp',
-	'eslint',
+  'eslint',
   'pyright',
+  'ruff_lsp',
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            },
-            codeLens = {
-              enabled = true
-            },
-            hint = {
-              enable = true
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      codeLens = {
+        enabled = true
+      },
+      hint = {
+        enable = true
+      }
     }
+  }
 })
-
--- vim.lsp.handlers["textDocument/codeAction"] = 
 
 -- https://github.com/typescript-language-server/typescript-language-server
 lsp.configure('tsserver', {
@@ -38,13 +37,13 @@ lsp.configure('tsserver', {
   --     logDirectory = './log',
   --   },
   -- },
-  on_attach = function ()
+  on_attach = function()
     print('tsserver attached')
   end,
 })
 
 lsp.configure('omnisharp', {
-  on_attach = function ()
+  on_attach = function()
     print('omnisharp attached')
   end
 })
@@ -55,14 +54,18 @@ lsp.configure('pyright', {
   settings = {
     python = {
       analysis = {
-        diagnosticMode = 'workspace'
+        diagnosticMode = 'workspace',
+        diagnosticSeverityOverrides = {
+          reportGeneralTypeIssues = "warning",
+          reportOptionalMemberAccess = false
+        }
       },
       venv = "venv",
       pythonPath = "python3"
       -- pythonPath = '/opt/homebrew/bin/python3.9'
     }
   },
-  on_attach = function ()
+  on_attach = function()
     print('pyright attached')
   end
 })
@@ -100,58 +103,28 @@ lsp.setup_nvim_cmp({
     format = function(_, vim_item)
       vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
       return vim_item
-    end,  }
-  })
+    end,
+  }
+})
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local lsp_format_on_save = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        -- vim.lsp.buf.formatting_sync()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
 
-  -- ** Use to print out client options ** --
-  -- local function tprint (tbl, indent)
-  --   if not indent then indent = 0 end
-  --   local toprint = string.rep(" ", indent) .. "{\r\n"
-  --   indent = indent + 2
-  --   for k, v in pairs(tbl) do
-  --     toprint = toprint .. string.rep(" ", indent)
-  --     if (type(k) == "number") then
-  --       toprint = toprint .. "[" .. k .. "] = "
-  --     elseif (type(k) == "string") then
-  --       toprint = toprint  .. k ..  "= "
-  --     end
-  --     if (type(v) == "number") then
-  --       toprint = toprint .. v .. ",\r\n"
-  --     elseif (type(v) == "string") then
-  --       toprint = toprint .. "\"" .. v .. "\",\r\n"
-  --     elseif (type(v) == "table") then
-  --       toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
-  --     else
-  --       toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
-  --     end
-  --   end
-  --   toprint = toprint .. string.rep(" ", indent-2) .. "}"
-  --   return toprint
-  -- end
-
-  -- if client.name == 'prettierd' then
-  --   print('found')
-  -- end
-
-  -- if client.name == 'tsserver' then
-
-    -- local printer = tprint(client, 2)
-
-    -- local file = io.open('ts_options', 'a')
-    -- io.output(file)
-    -- io.write(printer)
-    -- io.close(file)
-
-    -- print(printer)
-  -- end
-
-  -- if client.name == "eslint" then
-  --     vim.cmd.LspStop('eslint')
-  --     return
-  -- end
+local lsp_onattach_keymaps = function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   -- vim.keymap.set("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", opts)
@@ -166,13 +139,16 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
   vim.keymap.set("n", "<leader>bf", ":lua vim.lsp.buf.format { async = true }<CR>")
+end
+
+lsp.on_attach(function(client, bufnr)
+  lsp_format_on_save(client, bufnr)
+
+  lsp_onattach_keymaps(client, bufnr)
 end)
 
 lsp.setup()
 
--- vim.diagnostic.config({
--- 	vitual_text = true,
--- })
 vim.diagnostic.config({
   virtual_text = true,
   -- signs = true,
