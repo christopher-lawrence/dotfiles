8e546b9a909ae06cf2e177843cc52f09f36a867a
Christopher Lawrence <chrislawrence05@gmail.com>
2024-08-03 09:13:27 -0400

    switching back to dark mode...

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,12 +1,12 @@
 function ColorMyPencils(color)
 	-- color = color or "vscode"
-	-- color = color or "tokyonight"
+	color = color or "tokyonight"
 	-- color = color or "solarized"
 	-- color = color or "gruvbox"
-	color = color or "github_light"
+	-- color = color or "github_light"
 
 	vim.cmd.colorscheme(color)
-	vim.opt.background = "light"
+	vim.opt.background = "dark"
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
