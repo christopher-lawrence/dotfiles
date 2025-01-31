89f320108e5e5c1b1c5b875f0a4436ce55b4ec0b
Christopher Lawrence <chrislawrence05@gmail.com>
2025-01-31 16:28:57 -0500

    disable change detection

--- nvim/lua/lazy/lazy.lua
+++ nvim/lua/lazy/lazy.lua
@@ -33,4 +33,5 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
   checker = { enabled = true, notify = false },
+  change_detection = { enabled = false },
 })
