a904033a115390e05fe8349426124903e15e39cd
Christopher Lawrence <chrislawrence05@gmail.com>
2025-10-12 20:00:02 -0400

    swap cmp for blink.cmp

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -57,3 +57,46 @@ vim.keymap.set("n", "<leader>F", ":Format<cr>")
 vim.keymap.set("n", "<leader>gh", ":diffget //2<CR>")
 -- Select buffer 3 -- on the right -- during merge conflic / diff view
 vim.keymap.set("n", "<leader>gl", ":diffget //3<CR>")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		print("LspAttach event fired")
+
+		-- print(string.format('event fired: %s', vim.inspect(event)))
+		local opts = { buffer = event.buf, remap = false }
+
+		-- (W)orkspace
+		-- this is defined in telescope.lua
+		-- vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+
+		-- (D)iagnostics
+		-- Other diagnostics actions are defined in trouble.lua
+		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+
+		-- definitions/references
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+		-- code action
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+		-- utility
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
+
+		-- this triggers the buffer specific formatters, which can be different that Format.nvim for some reason
+		vim.keymap.set("n", "<leader>bf", ":lua vim.lsp.buf.format { async = true }<CR>")
+	end,
+})
+
+vim.diagnostic.config({
+	virtual_text = true,
+	-- signs = true,
+	-- update_in_insert = false,
+	-- underline = true,
+	-- severity_sort = false,
+	-- float = true,
+})
