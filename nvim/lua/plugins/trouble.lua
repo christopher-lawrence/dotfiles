f3b7557d86926883b329616cb6b7e006b4d4c93a
Christopher Lawrence <chrislawrence05@gmail.com>
2025-10-12 20:25:13 -0400

    better trouble setup

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -9,29 +9,55 @@ return {
 	-- 		-- refer to the configuration section below
 	-- 	})
 	-- end,
+
+	---@class trouble.Mode: trouble.Config,trouble.Section.spec
+	---@field desc? string
+	---@field sections? string[]
+
+	---@class trouble.Config
+	---@field mode? string
+	---@field config? fun(opts:trouble.Config)
+	---@field formatters? table<string,trouble.Formatter> custom formatters
+	---@field filters? table<string, trouble.FilterFn> custom filters
+	---@field sorters? table<string, trouble.SorterFn> custom sorters
 	opts = {
 		focus = true,
+		modes = {
+			split_references = {
+				mode = "lsp_references",
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.5,
+				},
+			},
+		},
 	},
 	keys = {
+    -- [g]o to [r]eferences
 		{
 			"<leader>gr",
-			"<cmd>Trouble lsp_references<cr>",
+			"<cmd>Trouble split_references<cr>",
 			desc = "Trouble: LSP References",
 		},
-		{
-			"<leader>dd",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Trouble: Toggle Buffer Diagnostics",
-		},
-		{
-			"<leader>D",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Trouble: Toggle Diagnostics",
-		},
+    -- [g]o to [d]efinition
 		{
 			"<leader>gd",
 			"<cmd>Trouble lsp_definitions<cr>",
 			desc = "Trouble: LSP Definitions",
+		},
+    -- [b]uffer [d]iagnostics
+		{
+			"<leader>bd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Trouble: Toggle Buffer Diagnostics",
+		},
+    -- [p]roject [d]iagnostics
+		{
+			"<leader>pd",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Trouble: Toggle Diagnostics",
 		},
 	},
 }
